806a1a3a1766c22b264b4b606ca98e8c6cbd82f2
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-03-07 10:43:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function timer() {
+  const time = new Date();
+  console.log(time);
+  const dateStr = time.toLocaleString();
+  document.getElementById("timer").textContent = dateStr;
+}
+setInterval(timer, 1000);
+
+timer();
